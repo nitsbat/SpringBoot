7f324dac642750ec2631fa3237908351ab9d609e
Nitin Bisht <3498georgian@gmail.com>
2021-02-14 18:08:20 +0530

    Using ApplicationContextAware interface to inject applicationContext

--- src/drawing/Rectangle.java
+++ src/drawing/Rectangle.java
@@ -1,9 +1,17 @@
 package drawing;
 
-public class Rectangle {
-    private Point point1;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-    public Rectangle(Point point1, Point point2, Point point3) {
+public class Rectangle implements ApplicationContextAware {
+    private Point point1;
+    private ApplicationContext context;
+
+    Rectangle() {
+    }
+
+    public Rectangle(Point point1) {
         this.point1 = point1;
     }
 
@@ -16,6 +24,13 @@ public class Rectangle {
     }
 
     public void draw() {
+        Point point2 = (Point) context.getBean("point3");
         System.out.println("Point A : (x=" + getPoint1().getX() + ",y=" + getPoint1().getY() + ")");
+        System.out.println("Point B : (x=" + point2.getX() + ",y=" + point2.getY() + ")");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
     }
 }
