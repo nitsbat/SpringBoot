2ebd16843e3e77db9317c8c9b02ac7127f6f17a1
Nitin Bisht <3498georgian@gmail.com>
2021-02-28 16:44:37 +0530

    Spring BeanPostProcessor

---
+++ src/drawing/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package drawing;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("in Before Initialization. Bean Name is " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After Initialization. Bean Name is " + beanName);
+        return bean;
+    }
+
+}
