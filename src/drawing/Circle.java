98fd20ea81f54172e8a5541006a4239f4df6c901
Nitin Bisht <3498georgian@gmail.com>
2021-03-28 23:16:17 +0530

    showed stereotype annotation with @repository as an example.

--- src/drawing/Circle.java
+++ src/drawing/Circle.java
@@ -1,9 +1,15 @@
 package drawing;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
+@Repository
 public class Circle implements Shape {
 
     private Point center;
