4f35ca60c344a1126c2d5cee10f4a620de9c057c
Nitin Bisht <3498georgian@gmail.com>
2021-03-29 18:16:41 +0530

    Part 4 - removing @ComponentScan

--- src/drawing/javaconfig/JavaConfig.java
+++ src/drawing/javaconfig/JavaConfig.java
@@ -2,11 +2,9 @@ package drawing.javaconfig;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "drawing.javaconfig")
 public class JavaConfig {
 
     @Bean
